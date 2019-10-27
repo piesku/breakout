f9eaa69e6904e12b4253005f56b6365462b91e5e
Michał Budzyński <michal@virtualdesign.pl>
2019-10-28 00:14:55 +0100

    brick fade out

--- src/blueprints/blu_brick.ts
+++ src/blueprints/blu_brick.ts
@@ -1,6 +1,7 @@
 import {collide} from "../components/com_collide.js";
 import {control_brick} from "../components/com_control_brick.js";
 import {draw_rect} from "../components/com_draw.js";
+import {fade} from "../components/com_fade.js";
 import {Blueprint2D} from "./blu_common.js";
 
 export function create_brick(width: number, height: number) {
@@ -9,6 +10,7 @@ export function create_brick(width: number, height: number) {
             control_brick(),
             collide(false, [width, height]),
             draw_rect(width, height, "green"),
+            fade(0),
         ],
     };
 }
--- src/systems/sys_control_brick.ts
+++ src/systems/sys_control_brick.ts
@@ -1,7 +1,7 @@
 import {Get, Has} from "../components/com_index.js";
 import {Entity, Game} from "../game.js";
 
-const QUERY = Has.Transform2D | Has.ControlBrick | Has.Collide;
+const QUERY = Has.Transform2D | Has.ControlBrick | Has.Collide | Has.Fade;
 
 export function sys_control_brick(game: Game, delta: number) {
     for (let i = 0; i < game.World.length; i++) {
@@ -13,8 +13,9 @@ export function sys_control_brick(game: Game, delta: number) {
 
 function update(game: Game, entity: Entity) {
     let collide = game[Get.Collide][entity];
+    let fade = game[Get.Fade][entity];
 
     if (collide.Collisions.length > 0) {
-        game.Destroy(entity);
+        fade.Step = 0.05;
     }
 }
