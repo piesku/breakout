31071674f28774c39233481f2f639f66629134f4
Staś Małolepszy <stas@piesku.com>
2019-10-30 16:58:44 +0100

    (sys_control_paddle) Move paddle with arrow keys

--- src/blueprints/blu_paddle.ts
+++ src/blueprints/blu_paddle.ts
@@ -1,8 +1,9 @@
 import {collide} from "../components/com_collide.js";
 import {control_paddle} from "../components/com_control_paddle.js";
 import {draw_rect} from "../components/com_draw.js";
+import {move} from "../components/com_move.js";
 import {Blueprint2D} from "./blu_common.js";
 
 export let paddle_blueprint = <Blueprint2D>{
-    Using: [control_paddle(100), collide(true, [100, 20]), draw_rect(100, 20, "red")],
+    Using: [control_paddle(100), move(500), collide(true, [100, 20]), draw_rect(100, 20, "red")],
 };
--- src/systems/sys_control_paddle.ts
+++ src/systems/sys_control_paddle.ts
@@ -1,7 +1,7 @@
 import {Get, Has} from "../components/com_index.js";
 import {Entity, Game} from "../game.js";
 
-const QUERY = Has.Transform2D | Has.ControlPaddle;
+const QUERY = Has.Transform2D | Has.ControlPaddle | Has.Move;
 
 export function sys_control_paddle(game: Game, delta: number) {
     for (let i = 0; i < game.World.length; i++) {
@@ -14,14 +14,21 @@ export function sys_control_paddle(game: Game, delta: number) {
 function update(game: Game, entity: Entity) {
     let transform = game[Get.Transform2D][entity];
     let control = game[Get.ControlPaddle][entity];
+    let move = game[Get.Move][entity];
 
-    let x = transform.Translation[0] + game.InputEvent.mouse_x;
+    let x = transform.Translation[0];
     if (x < control.Width / 2) {
         transform.Translation[0] = control.Width / 2;
+        transform.Dirty = true;
     } else if (game.ViewportWidth - control.Width / 2 < x) {
         transform.Translation[0] = game.ViewportWidth - control.Width / 2;
+        transform.Dirty = true;
     } else {
-        transform.Translation[0] = x;
+        if (game.InputState.ArrowLeft && !game.InputState.ArrowRight) {
+            move.Direction = [-1, 0];
+        }
+        if (game.InputState.ArrowRight && !game.InputState.ArrowLeft) {
+            move.Direction = [1, 0];
+        }
     }
-    transform.Dirty = true;
 }
