660d10b9f237c2ba9e44c6dbed728a497618a907
Staś Małolepszy <stas@piesku.com>
2019-11-03 21:18:38 +0100

    Remove input from sys_control_paddle

--- src/systems/sys_control_paddle.ts
+++ src/systems/sys_control_paddle.ts
@@ -1,7 +1,6 @@
 import {Get, Has} from "../components/com_index.js";
 import {Entity, Game} from "../game.js";
 import {Vec2} from "../math/index.js";
-import {add, normalize} from "../math/vec2.js";
 
 const QUERY = Has.Transform2D | Has.ControlPaddle;
 
@@ -16,21 +15,6 @@ export function sys_control_paddle(game: Game, delta: number) {
 function update(game: Game, entity: Entity, delta: number) {
     let direction = <Vec2>[0, 0];
     let speed = 300;
-
-    if (game.InputState.ArrowLeft) {
-        add(direction, direction, [-1, 0]);
-    }
-    if (game.InputState.ArrowRight) {
-        add(direction, direction, [1, 0]);
-    }
-    if (game.InputState.ArrowUp) {
-        add(direction, direction, [0, -1]);
-    }
-    if (game.InputState.ArrowDown) {
-        add(direction, direction, [0, 1]);
-    }
-
-    normalize(direction, direction);
 
     let transform = game[Get.Transform2D][entity];
     transform.Translation[0] += direction[0] * speed * delta;
