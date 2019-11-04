1984b077b85da551d6fa1271c63b09a34d560cec
Staś Małolepszy <stas@piesku.com>
2019-11-04 21:49:52 +0100

    (2h) Bounce the ball off the walls

--- src/systems/sys_control_ball.ts
+++ src/systems/sys_control_ball.ts
@@ -2,7 +2,7 @@ import {Has} from "../components/com_index.js";
 import {Entity, Game} from "../game.js";
 import {normalize} from "../math/vec2.js";
 
-const QUERY = Has.ControlBall | Has.Move;
+const QUERY = Has.Transform2D | Has.ControlBall | Has.Move;
 
 export function sys_control_ball(game: Game, delta: number) {
     for (let i = 0; i < game.World.Mask.length; i++) {
@@ -14,6 +14,28 @@ export function sys_control_ball(game: Game, delta: number) {
 
 function update(game: Game, entity: Entity) {
     let control = game.World.ControlBall[entity];
+    let transform = game.World.Transform2D[entity];
+
+    if (transform.Translation[0] < 0) {
+        transform.Translation[0] = 0;
+        control.Direction[0] = -control.Direction[0];
+    }
+
+    if (transform.Translation[0] > game.ViewportWidth) {
+        transform.Translation[0] = game.ViewportWidth;
+        control.Direction[0] = -control.Direction[0];
+    }
+
+    if (transform.Translation[1] < 0) {
+        transform.Translation[1] = 0;
+        control.Direction[1] = -control.Direction[1];
+    }
+
+    if (transform.Translation[1] > game.ViewportHeight) {
+        transform.Translation[1] = game.ViewportHeight;
+        control.Direction[1] = -control.Direction[1];
+    }
+
     let move = game.World.Move[entity];
     move.Direction[0] = control.Direction[0];
     move.Direction[1] = control.Direction[1];
